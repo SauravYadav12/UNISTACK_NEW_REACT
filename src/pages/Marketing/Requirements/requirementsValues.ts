d29eb3c72eb8740a8a957675cab06444c551ef68
SauravYadav12 <saurav1286@gmail.com>
2025-01-27 23:46:54 +0530

    Added footer and requirement form issue - Fixed

--- src/pages/Marketing/Requirements/requirementsValues.ts
+++ src/pages/Marketing/Requirements/requirementsValues.ts
@@ -5,24 +5,62 @@ export const requestStatusOptions = [
     'Cancelled',
     'Project Active',
     'Project Inactive',
-  ];
+];
 export const assignedToOptions = [
-    "Saurav Yadav", 
-    "Navendra Yadav",
+    "Saurav Yadav",
+    "Anmol",
     "Sam",
     "Pramod",
-    "Ankush"
+    "Navendra Yadav",
+    "Ankush",
+    "Rahul RR",
+    "Alina",
+    "Shivam Tiwari",
+    "Ravindra Joshi",
+    "Aditi",
+    "Khushboo",
+    "Saloni Yadav",
+    "Aman Rajawat",
+    "Bhupendra Verma",
+    "Rohit Singh",
+    "Yashwant Thakur"
 ];
 export const appliedForOptions = [
-    "Software Engineer",
-    "Backend Developer"
+    "Not in use",
+    "Taya Veer",
+    "TayaNithi Veer",
+    "Ben Hanna",
+    "Ambigayi Mathavan",
+    "Kuldeep Kumar Verma",
+    "Allan Robinson",
+    "Harsh Rana",
+    "Basant Kumar Sapkota",
+    "Kevin Singh",
+    "Doug Howard",
+    "Divyang Parikh",
+    "YagyaVeer Singh",
+    "Mina",
+    "Bibek Humagain",
+    "Nash S",
+    "Ash Veni",
+    "MD Yousuf",
+    "Meena Youssef",
+    "Taya Veeraya",
+    "Yagya Vir Singh",
+    "Sujan Kumar Bishwokarma",
+    "Sjob B [Pronounce as Shawn]",
+    "Nitin Sharma",
+    "Mina Y",
+    "Nithi Veer",
+    "Kinnar Patel",
+    "Aradhana Singh",
 ];
 export const taxTypeOptions = [
     "C2C",
     "1099",
     "W2 With vendor",
     "W2 With client"
-    ];
+];
 
 export const gotRequirementForm = [
     "Got from online Resume",
@@ -51,4 +89,11 @@ export const techStack = [
     "QA/Tester",
     "Azure",
     "Other Frontend"
+]
+
+export const duration = [
+    "03 Months",
+    "06 Months",
+    "12 Months",
+    "12+ Months",
 ]
