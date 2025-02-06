a1d650da6a4f299107010129789a498e3f5eef4d
anurag-unicodez <anurag.dhurwey@unicodez.com>
2025-02-07 01:59:29 +0530

    dashboard report api integration --- done

--- src/services/reportsApi.ts
+++ src/services/reportsApi.ts
@@ -5,22 +5,27 @@ import {
   SupportReport,
   InterviewReport,
   MarketingReport,
+  DashboardReport,
 } from '../Interfaces/reports';
 
 const BASE_URL: string = import.meta.env.VITE_API_BASE_URL;
 
 export async function getReport<T>(
-  type: 'marketing' | 'support' | 'interview',
-  fromDate: string,
-  toDate: string
+  type: 'marketing' | 'support' | 'interview' | 'dashboard',
+  fromDate?: string,
+  toDate?: string
 ) {
   const token = await getJwtToken();
   let headers: any = {
     'Content-Type': 'application/json',
     Authorization: token,
   };
+  let query = '';
+  if (fromDate) query += `fromDate=${fromDate}&`;
+  if (toDate) query += `toDate=${toDate}`;
+
   const response = await axios.get<ApiQueryRes<T>>(
-    `${BASE_URL}/reports/${type}?fromDate=${fromDate}&toDate=${toDate}`,
+    `${BASE_URL}/reports/${type}?${query}`,
     {
       headers,
     }
@@ -52,4 +57,7 @@ export async function getInterviewReport(
     report: InterviewReport[];
     totalInterviews: number;
   }>('interview', fromDate, toDate);
+}
+export async function getDashboardReport() {
+  return await getReport<DashboardReport>('dashboard');
 }
